a040df9befe1258812a6fe87eaeb663de9e4454d
Andrew John Burke <s10266774@connect.np.edu.sg>
2025-12-05 15:39:53 +0800

    Login System implementation
    > Login system UI
    > Login system checker
    > Error code & Scene change implementation

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -1,15 +1,53 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.SceneManagement;
 
-public class Login : MonoBehaviour
+
+public class DataManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // TM textfields;
+    public TMP_InputField EmailInput;
+    public TMP_InputField PasswordInput;
+    public TMP_Text validationText;
+    
+    private string usernameInput = "admintest";
+    private string passwordInput = "admintest";
+
+    private IEnumerator loadScene()
     {
-        
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("SampleScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator delay()
+    {
+        yield return new WaitForSeconds(4);
+    }
+
+    public void loginUser()
+    {
+       if (EmailInput.text == usernameInput && PasswordInput.text == passwordInput)
+        {
+            validationText.text = "Login Successful!";
+            
+            StartCoroutine(loadScene());
+
+        }
+        else
+        {
+            validationText.text = "Login Failed. Please try again.";
+
+            // StartCoroutine(delay());
+
+            // validationText.text = "";
+
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
         
     }
